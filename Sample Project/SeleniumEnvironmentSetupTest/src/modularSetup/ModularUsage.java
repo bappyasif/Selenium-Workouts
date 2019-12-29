7ce83e445980b58a48f3f97ecda3855165b2ae31
BaPpY <bappyasif@hotmail.com>
2019-12-29 22:03:10 +0600

    Added TO Previous Commit With Config Properties File Usage Along With JAR Creation And Running From CMD Examples.

--- Sample Project/SeleniumEnvironmentSetupTest/src/modularSetup/ModularUsage.java
+++ Sample Project/SeleniumEnvironmentSetupTest/src/modularSetup/ModularUsage.java
@@ -1,24 +1,38 @@
 package modularSetup;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import configSettings.PropertiesFileSettingsSetup;
+
 public class ModularUsage {
 
-	static String browserPlatform; 
+	public static String browserPlatform; 
 	static WebDriver web_driver;
 	
 	static String base_URL = "https://www.google.com";
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+//		PropertiesFileSettingsSetup.readFromPropertiesFile();
+//		browserConfig();
+//		commenceTest();
+//		closeBrowser();
+//		PropertiesFileSettingsSetup.writeIntoPropertiesFile();
+		
 		ModularUsage createdObject = new ModularUsage();
 		
-		createdObject.setBrowser();
+		//createdObject.setBrowser();
+		PropertiesFileSettingsSetup.readFromPropertiesFile();
 		createdObject.browserConfig();
 		createdObject.commenceTest();
 		createdObject.closeBrowser();
+		PropertiesFileSettingsSetup.writeIntoPropertiesFile();
 	}
 	
 	
@@ -35,7 +49,8 @@ public class ModularUsage {
 	public static void browserConfig() {
 		// Project Location
 		String projectLocation = System.getProperty("user.dir");
-
+		//System.out.println(browserPlatform);
+		
 		if(browserPlatform.contains("Chrome")) {
 			// Chrome Settings
 			System.setProperty("webdriver.chrome.driver", projectLocation+"\\Lib\\WebDrivers\\chromedriver.exe");
